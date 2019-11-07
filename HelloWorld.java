e1b71bbb5ea5a470b96a2853bb48d403e100852e
afejzullahu <afejzullahu@citycollege.sheffield.eu>
2019-11-07 14:20:59 +0200

    HelloWorld.java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld{
 	public static void main (String [] args){
 		System.out.println("HelloWorld");
-		for ( int i = 0; i < 10;i++ ) {
+		int i = 5;
+		while (i > 0){
 	System.out.println("HelloWorld");
 		}
 	}			
